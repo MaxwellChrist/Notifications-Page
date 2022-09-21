93ee1d65c047ddf25e1124af400db9cc914f74d6
MaxwellChrist <ribthulu@gmail.com>
2022-09-21 16:10:50 -0400

    Added typescript to toggle classes

--- notifications-page-main/public/index.js
+++ notifications-page-main/public/index.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log("fffff");
+function markAllAsRead() {
+    let activeClasses = document.querySelectorAll(".notification-body-container");
+    let activeIcons = document.querySelectorAll(".notification-read-toggle");
+    let newActiveClasses = Array.from(activeClasses);
+    let newActiveIcons = Array.from(activeIcons);
+    newActiveClasses.forEach((item, index) => {
+        if (item.classList.contains("active") && index <= 2) {
+            item.classList.remove("active");
+        }
+        else if (index <= 2) {
+            item.classList.add("active");
+        }
+    });
+    newActiveIcons.forEach((item) => {
+        if (item.classList.contains("notification-read-toggle-disable")) {
+            item.classList.remove("notification-read-toggle-disable");
+        }
+        else {
+            item.classList.add("notification-read-toggle-disable");
+        }
+    });
+}
--- notifications-page-main/src/index.ts
+++ notifications-page-main/src/index.ts
@@ -1,1 +1,23 @@
-console.log("fffff")
+function markAllAsRead(): void {
+    let activeClasses: NodeList = document.querySelectorAll(".notification-body-container") as NodeList
+    let activeIcons: NodeList = document.querySelectorAll(".notification-read-toggle") as NodeList
+
+    let newActiveClasses: HTMLDivElement[] = Array.from(activeClasses) as HTMLDivElement[]
+    let newActiveIcons = Array.from(activeIcons) as HTMLDivElement[]
+
+    newActiveClasses.forEach((item: HTMLDivElement, index: number): void => {
+        if (item.classList.contains("active") && index <= 2) {
+            item.classList.remove("active")
+        } else if (index <= 2){
+            item.classList.add("active")
+        }
+    })
+
+    newActiveIcons.forEach((item: HTMLSpanElement): void => {
+        if (item.classList.contains("notification-read-toggle-disable")) {
+            item.classList.remove("notification-read-toggle-disable")
+        } else {
+            item.classList.add("notification-read-toggle-disable")
+        }
+    })
+}
